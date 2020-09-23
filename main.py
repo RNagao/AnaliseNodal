ffc429b310e50e7df3c64006950577b22a64fa24
Raphael Nagao <nagao1234@poli.ufrj.br>
2020-09-23 04:00:08 -0300

    correcao bug reg sen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import sympy as sp
 import numpy as np
+
 
 #funcoes para a analise dos componentes em cada linha da netlist
 def decideComponente(linhaComponentData):
@@ -12,32 +14,48 @@ file = open(fileName, 'r')
 content = file.readlines()
 
 componentsData = []
-w = 0
+w = 1
 nodals = 0
 modificada = 0
+regSen = False
+t = sp.Symbol("t")
 for i in range(len(content)):
     #remover final \n das linhas do content
     content[i] = content[i][0:-1] 
     #adiciona uma lista com os dados de cada componente na matriz component
     componentsData += [content[i].split()]
+
+for i in range(len(componentsData)):
     #checa o tamanho da matriz e a frequencia(w)
     try:
-        if float(componentsData[i][1]) > nodals:
-            nodals = int(componentsData[i][1])
-        if float(componentsData[i][2]) > nodals:
-            nodals = int(componentsData[i][2])
+        if componentsData[i][0][0] == "K":
+            if int(componentsData[i][2]) > nodals:
+                nodals = int(componentsData[i][2])
+            if int(componentsData[i][3]) > nodals:
+                nodals = int(componentsData[i][3])
+            if int(componentsData[i][6]) > nodals:
+                nodals = int(componentsData[i][6])
+            if int(componentsData[i][7]) > nodals:
+                nodals = int(componentsData[i][7])
+        else:  
+            if int(componentsData[i][1]) > nodals:
+                nodals = int(componentsData[i][1])
+            if int(componentsData[i][2]) > nodals:
+                nodals = int(componentsData[i][2])
         if (componentsData[i][0][0] == "V") or (componentsData[i][0][0] == "E") or (componentsData[i][0][0] == "H"):
             modificada += 1
-        if componentsData[i][0][0] == ("H"):
+        if componentsData[i][0][0] == "H":
             modificada += 2
         if (componentsData[i][0][0] == "V") or (componentsData[i][0][0] == "I"):
-            w = 2*(np.pi)*float(componentsData[i][6])
+            if componentsData[i][3] == "SIN":
+                w = 2*(np.pi)*complex(componentsData[i][6])
+                regSen = True
     except:
         print("Linha " + str(i) + " não foi reconhecida como um componente")
 
 #monta matrizes zeradas de acordo com o numero de nos
-G = np.zeros((nodals + modificada + 1, nodals + modificada + 1))
-I = np.zeros(nodals + modificada + 1)
+G = np.zeros((nodals + modificada + 1, nodals + modificada + 1), dtype="complex_")
+I = np.zeros(nodals + modificada + 1, dtype="complex_")
 
 #monta a matriz de acordo com os componentes dados
 indiceMod = 1
@@ -45,47 +63,49 @@ for i in range(len(content)):
     nome = componentsData[i][0][0]
 
     if nome == "R": #resistor
-        G[int(componentsData[i][2])][int(componentsData[i][2])] += 1/float(componentsData[i][3])
-        G[int(componentsData[i][1])][int(componentsData[i][1])] += 1/float(componentsData[i][3])
-        G[int(componentsData[i][1])][int(componentsData[i][2])] += -1/float(componentsData[i][3])
-        G[int(componentsData[i][2])][int(componentsData[i][1])] += -1/float(componentsData[i][3])
+        G[int(componentsData[i][2])][int(componentsData[i][2])] += 1/complex(componentsData[i][3])
+        G[int(componentsData[i][1])][int(componentsData[i][1])] += 1/complex(componentsData[i][3])
+        G[int(componentsData[i][1])][int(componentsData[i][2])] += -1/complex(componentsData[i][3])
+        G[int(componentsData[i][2])][int(componentsData[i][1])] += -1/complex(componentsData[i][3])
 
 
     elif nome == "I": #fonte de corrente independente
         if componentsData[i][3] == "DC":
-            I[int(componentsData[i][2])] += float(componentsData[i][4]) 
-            I[int(componentsData[i][1])] += -float(componentsData[i][4])
+            I[int(componentsData[i][2])] += complex(componentsData[i][4]) 
+            I[int(componentsData[i][1])] += -complex(componentsData[i][4])
 
         elif componentsData[i][3] == "SIN":
-            Ao = componentsData[i][4]
-            A = componentsData[i][5]
-            f = componentsData[i][6]
-            ta = componentsData[i][7]
-            a = componentsData[i][7]
-            fi = componentsData[i][8]
-            pi = np.pi
-            exp = np.exp(-a*(t-ta))
-            valor = Ao + (A*exp) * np.sin(2*pi*f*(t-ta) + (pi/180)*fi)
-            I[int(componentsData[i][2])] += valor
-            I[int(componentsData[i][1])] += -valor
+            Ao = complex(componentsData[i][4])
+            A = complex(componentsData[i][5])
+            f = complex(componentsData[i][6])
+            ta = complex(componentsData[i][7])
+            a = complex(componentsData[i][7])
+            fi = complex(componentsData[i][8])
+            pi = sp.pi
+            exp = sp.exp(-a*(t-ta))
+            #valor = Ao + (A*exp) * sp.sin(2*pi*f*(t-ta) + (pi/180)*fi)
+            fase = (pi/180) * fi
+            fasor = A*(sp.cos(fase)+1j*sp.sin(fase))
+            I[int(componentsData[i][2])] += fasor
+            I[int(componentsData[i][1])] += -fasor
 
     elif nome == "G": #fonte de corrente controlada por tensão
-        G[int(componentsData[i][1])][int(componentsData[i][3])] += -float(componentsData[i][5])
-        G[int(componentsData[i][1])][int(componentsData[i][4])] += float(componentsData[i][5])
-        G[int(componentsData[i][2])][int(componentsData[i][3])] += float(componentsData[i][5])
-        G[int(componentsData[i][2])][int(componentsData[i][4])] += -float(componentsData[i][5])
+        G[int(componentsData[i][1])][int(componentsData[i][3])] += -complex(componentsData[i][5])
+        G[int(componentsData[i][1])][int(componentsData[i][4])] += complex(componentsData[i][5])
+        G[int(componentsData[i][2])][int(componentsData[i][3])] += complex(componentsData[i][5])
+        G[int(componentsData[i][2])][int(componentsData[i][4])] += -complex(componentsData[i][5])
 
     elif nome == "C": #Capacitor
-        G[int(componentsData[i][1])][int(componentsData[i][1])] += 1j*w*float(componentsData[i][3])
-        G[int(componentsData[i][1])][int(componentsData[i][2])] += -1j*w*float(componentsData[i][3])
-        G[int(componentsData[i][2])][int(componentsData[i][1])] += -1j*w*float(componentsData[i][3])
-        G[int(componentsData[i][2])][int(componentsData[i][2])] += 1j*w*float(componentsData[i][3])
+        G[int(componentsData[i][1])][int(componentsData[i][1])] += complex(1j*w*complex(componentsData[i][3]))
+        G[int(componentsData[i][1])][int(componentsData[i][2])] += complex(-1j*w*complex(componentsData[i][3]))
+        G[int(componentsData[i][2])][int(componentsData[i][1])] += complex(-1j*w*complex(componentsData[i][3]))
+        G[int(componentsData[i][2])][int(componentsData[i][2])] += complex(1j*w*complex(componentsData[i][3]))
 
     elif nome == "L": #Indutor
-        G[int(componentsData[i][1])][int(componentsData[i][1])] += 1/(1j*w*float(componentsData[i][3]))
-        G[int(componentsData[i][1])][int(componentsData[i][2])] += -1/(1j*w*float(componentsData[i][3]))
-        G[int(componentsData[i][2])][int(componentsData[i][1])] += -1/(1j*w*float(componentsData[i][3]))
-        G[int(componentsData[i][2])][int(componentsData[i][2])] += 1/(1j*w*float(componentsData[i][3]))
+        G[int(componentsData[i][1])][int(componentsData[i][1])] += complex(1/(1j*w*complex(componentsData[i][3])))
+        G[int(componentsData[i][1])][int(componentsData[i][2])] += complex(-1/(1j*w*complex(componentsData[i][3])))
+        G[int(componentsData[i][2])][int(componentsData[i][1])] += complex(-1/(1j*w*complex(componentsData[i][3])))
+        G[int(componentsData[i][2])][int(componentsData[i][2])] += complex(1/(1j*w*complex(componentsData[i][3])))
 
 
     elif nome == "V": #Fonte de tensao
@@ -94,25 +114,27 @@ for i in range(len(content)):
             G[int(componentsData[i][2])][nodals + indiceMod] += -1
             G[nodals + indiceMod][int(componentsData[i][1])] += -1
             G[nodals + indiceMod][int(componentsData[i][2])] += 1
-            I[nodals + indiceMod] += -float(componentsData[i][4])
+            I[nodals + indiceMod] += -complex(componentsData[i][4])
             print("J" + str(indiceMod) + " => " + componentsData[i][0])
             indiceMod += 1
         
         elif componentsData[i][3] == "SIN":
-            Ao = componentsData[i][4]
-            A = componentsData[i][5]
-            f = componentsData[i][6]
-            ta = componentsData[i][7]
-            a = componentsData[i][7]
-            fi = componentsData[i][8]
-            pi = np.pi
-            exp = np.exp(-a*(t-ta))
-            valor = Ao + (A*exp) * np.sin(2*pi*f*(t-ta) + (pi/180)*fi)
+            Ao = complex(componentsData[i][4])
+            A = complex(componentsData[i][5])
+            f = complex(componentsData[i][6])
+            ta = complex(componentsData[i][7])
+            a = complex(componentsData[i][7])
+            fi = complex(componentsData[i][8])
+            pi = sp.pi
+            exp = sp.exp((-1)*a*(t-ta))
+            #valor = Ao + (A*exp) * sp.sin(2*pi*f*(t-ta) + (pi/180)*fi)
+            fase = (pi/180) * fi
+            fasor = A*(sp.cos(fase)+1j*sp.sin(fase))
             G[int(componentsData[i][1])][nodals + indiceMod] += 1
             G[int(componentsData[i][2])][nodals + indiceMod] += -1
             G[nodals + indiceMod][int(componentsData[i][1])] += -1
             G[nodals + indiceMod][int(componentsData[i][2])] += 1
-            I[nodals + indiceMod] += -valor
+            I[nodals + indiceMod] += fasor
             print("J" + str(indiceMod) + " => " + componentsData[i][0])
             indiceMod += 1
     
@@ -122,14 +144,14 @@ for i in range(len(content)):
         G[int(componentsData[i][2])][nodals + indiceMod] += -1
         G[nodals + indiceMod][int(componentsData[i][1])] += -1
         G[nodals + indiceMod][int(componentsData[i][2])] += 1
-        G[nodals + indiceMod][int(componentsData[i][3])] += float(componentsData[i][5])
-        G[nodals + indiceMod][int(componentsData[i][4])] += -float(componentsData[i][5])
+        G[nodals + indiceMod][int(componentsData[i][3])] += complex(componentsData[i][5])
+        G[nodals + indiceMod][int(componentsData[i][4])] += -complex(componentsData[i][5])
         print("J" + str(indiceMod) + " => " + componentsData[i][0])
         indiceMod += 1
     
     elif nome == "F": #Fonte de corrente controlada por corrente
-        G[int(componentsData[i][1])][nodals + indiceMod] += float(componentsData[i][5])
-        G[int(componentsData[i][2])][nodals + indiceMod] += -float(componentsData[i][5])
+        G[int(componentsData[i][1])][nodals + indiceMod] += complex(componentsData[i][5])
+        G[int(componentsData[i][2])][nodals + indiceMod] += -complex(componentsData[i][5])
         G[int(componentsData[i][3])][nodals + indiceMod] += 1
         G[int(componentsData[i][4])][nodals + indiceMod] += -1
         G[nodals + indiceMod][int(componentsData[i][3])] += -1
@@ -146,18 +168,18 @@ for i in range(len(content)):
         G[nodals + indiceMod][int(componentsData[i][4])] += 1
         G[nodals + indiceMod + 1][int(componentsData[i][1])] += -1
         G[nodals + indiceMod + 1][int(componentsData[i][2])] += 1
-        G[nodals + indiceMod + 1][nodals + indiceMod] += float(componentsData[i][5])
+        G[nodals + indiceMod + 1][nodals + indiceMod] += complex(componentsData[i][5])
         print("J" + str(indiceMod) + " => " + componentsData[i][0] + " Control")
         print("J" + str(indiceMod + 1) + " => " + componentsData[i][0] + " Source")
         indiceMod += 2
     
     elif nome == "K": #transformador ideal
-        L1 = componentsData[i][4]
-        L2 = componentsData[i][8]
-        M = componentsData[i][9]
-        L11 = L2/(L1*L2 - (M^2))
-        L22 = L1/(L1*L2 - (M^2))
-        L12 = -M/(L1*L2 - (M^2))
+        L1 = complex(componentsData[i][4])
+        L2 = complex(componentsData[i][8])
+        M = complex(componentsData[i][9])
+        L11 = L2/(L1*L2 - (M*M))
+        L22 = L1/(L1*L2 - (M*M))
+        L12 = -M/(L1*L2 - (M*M))
         G[int(componentsData[i][2])][int(componentsData[i][2])] += L11/(1j*w)
         G[int(componentsData[i][2])][int(componentsData[i][3])] += -L11/(1j*w)
         G[int(componentsData[i][2])][int(componentsData[i][6])] += L12/(1j*w)
@@ -180,6 +202,7 @@ G = np.array(G)
 I = np.array(I)
 
 #apaga a linha e coluna 0 do no ground
+
 G = np.delete(G, 0, axis=0)
 G = np.delete(G, 0, axis=1)
 I = np.delete(I, 0)
@@ -188,10 +211,33 @@ I = np.delete(I, 0)
 E = np.linalg.solve(G, I)
 
 #printa os valores das tensões nodais
+#if regSen == False:
 for i in range (nodals):
     print("V" + str(i+1) + "= " + str(E[i]))
-for i in range (modificada):
-    print("J" + str(i+1) + "= " + str(E[i]))
+'''
+else:
+    for i in range (nodals):
+        parentesesInicial = 0
+        signCount = 0
+        real = ""
+        img = ""
+        V = str(E[i])
+        for n in range (len(V) - 1):
+            if parentesesInicial == 1:
+                if (((str(V[n]) == "-")  or (str(V[n]) == "+")) and str(V[n-1]) != "e"):
+                    signCount += 1
+                if signCount == 2:
+                    img += str(V[n])
+                else:
+                    real += str(V[n])
+            else:
+                parentesesInicial += 1
+        if img[0] == "-":
+            img[0] = "+"
+        else:
+            img[0] = "-"
+        print("V" + str(i+1) + "= " + real + "*cos(" + str(w) + "*t)" + img + "*sin(" + str(w) + "*t")
+'''
 
 
 
